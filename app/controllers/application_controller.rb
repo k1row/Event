b934b21a2eb83569d63db5454936dce168f9c41f
Keiichiro Nagashima <k16.nagashima@gmail.com>
2014-06-26 13:50:32 +0900

    add error hander

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,19 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?
 
+  rescue_from Exception, with: :error500
+  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+
+  def error404
+    render 'error404', status: 404, formats: [:html]
+  end
+
+  def error500(e)
+    # エラーの種類とバックトレースの出力
+    logger.error [e, *e.backtrace].join("\n")
+    render 'error500', status: 500, formats: [:html]
+  end
+
   private
   def logged_in?
   	!!session[:user_id]
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
 
-  resource :user do
+  resource :user, only: :destory do
   	get 'retire'
   end
 
-  resources :events do
-    resources :tickets
+  resources :events, except: :index do
+    resources :tickets, only: [:new, :create, :destroy]
   end
+
+  # すべてのURLをキャッチする
+  match '*path' => 'application#error404', via: :all
 end
